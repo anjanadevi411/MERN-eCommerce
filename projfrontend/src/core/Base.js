eb402292bfabea6def06db4c8bb7831a38c84783
Anjana Devi <anjanadevi411@gmail.com>
2022-01-30 22:18:52 +0100

    introduced the base component

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Base({
+  title = "My title",
+  description = "My description",
+  className = "text-white bg-dark p-4",
+  children,
+}) {
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="jumbotron bg-dark text-white text-center">
+          <h2 className="display-4">{title}</h2>
+          <p className="lead">{description}</p>
+        </div>
+        <div className={className}>{children}</div>
+      </div>
+      <footer className="footer bg-dark mt-auto py-3">
+        <div className="container-fluid bg-success text-white text-center py-3">
+          <h4>If you have any query's. Please feel free to contact us</h4>
+          <button className="btn btn-warning btn-lg">Contact US</button>
+        </div>
+        <div className="container">
+          <span className="text-muted">
+            {" "}
+            An Amazing place to shop online{" "}
+            <span className="text-white">T-shirts</span>
+          </span>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Base;
